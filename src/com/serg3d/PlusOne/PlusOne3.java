babdece10cfd9fc6a624ffa7c35b6e22ba270a5b
Sergey Okinchuk <serg3d@hotmail.com>
2016-09-18 00:28:41 -0700

    PlusOne

--- src/com/serg3d/PlusOne/PlusOne3.java
+++ src/com/serg3d/PlusOne/PlusOne3.java
@@ -9,7 +9,9 @@ public class PlusOne3 {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 3, 5, 7, 2 , 1};
+        //int[] arr = {9, 3, 5, 7, 2 , 1};
+        //int[] arr = {9, 9, 9, 9, 9 , 9};
+        int[] arr = {9, 9};
         //int[] arr = {0};
 
         arr = plusOne(arr);
@@ -36,18 +38,28 @@ public class PlusOne3 {
 
         int increment = 0;
         for (int i = digits.length - 1; i > -1; i--) {
-            if (i == digits.length) {
-                digits[i] = (++digits[i]);
+            if (i == digits.length - 1) {
+                digits[i] = digits[i] + 1;
             }
             digits[i] = digits[i] + increment;
             increment = 0;
             if (digits[i] > 9) {
                 increment++;
                 digits[i] = digits[i] % 10;
-                System.out.println(digits[i]);
+                //System.out.println(digits[i]);
+            }
+
+            if (increment > 0) {
+                int[] moreDigits = new int[digits.length + 1];
+                moreDigits[0] = increment;
+                for (int j = 1; j < moreDigits.length; j++) {
+                    moreDigits[j] = digits[j-1];
+                }
+                return moreDigits;
             }
 
         }
+
 
 
 
