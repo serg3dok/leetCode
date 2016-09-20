15556905f3dffc9178a568c78ab7724a2f483e46
Sergey Okinchuk <serg3d@hotmail.com>
2016-09-20 00:20:19 -0700

    work on LengthLongestPath

--- src/com/serg3d/LengthLongestPath/Solution2.java
+++ src/com/serg3d/LengthLongestPath/Solution2.java
@@ -4,7 +4,7 @@ package com.serg3d.LengthLongestPath;
 /**
  * Created by Sergey on 9/18/2016.
  */
-public class Solution {
+public class Solution2 {
     public static void main(String[] args)
     {
         String path = "dir\\n\\tsubdir1\\n\\t\\tfile1.ext\\n\\t\\tsubsubdir1\\n\\tsubdir2\\n\\t\\tsubsubdir2\\n\\t\\t\\tfile2.ext";
