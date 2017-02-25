14432be8c2dedb62cc7db8a5125a18900b0bd9c9
Sergey Okinchuk <serg3d@hotmail.com>
2017-02-24 23:14:11 -0800

    solved KthLargestElementInArray

--- src/com/serg3d/KthLargestElementInArray/KthLargestElementInArray.java
+++ src/com/serg3d/KthLargestElementInArray/KthLargestElementInArray.java
@@ -1,9 +1,31 @@
 package com.serg3d.KthLargestElementInArray;
 
+import java.util.Arrays;
+
 /**
  * Created by sergeyo on 2/24/2017.
  */
 public class KthLargestElementInArray {
 
 
+        public static void main(String[] args) {
+            System.out.println(findKthLargest(new int[] {-1, -1}, 2));
+        }
+
+
+        public static int findKthLargest(int[] nums, int k) {
+
+
+            int[] arr = new int[nums.length/k];
+
+
+            for (int i = k-1, j = 0; i < nums.length; i=i+k, j++) {
+                arr[j] = nums[i];
+            }
+
+            Arrays.sort(arr);
+
+            return(arr[arr.length-1]);
+
+        }
 }
